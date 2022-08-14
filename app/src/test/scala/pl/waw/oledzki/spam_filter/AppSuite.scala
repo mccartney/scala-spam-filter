39dbe7ecd09ba4cb7bca77a9d32404d4677473d4
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2022-08-14 12:52:30 +0200

    Removing test

--- app/src/test/scala/pl/waw/oledzki/spam_filter/AppSuite.scala
+++ app/src/test/scala/pl/waw/oledzki/spam_filter/AppSuite.scala
@@ -1,12 +1,9 @@
 package pl.waw.oledzki.spam_filter
 
-import org.scalatest.funsuite.AnyFunSuite
 import org.junit.runner.RunWith
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends AnyFunSuite {
-  test("App has a greeting") {
-    assert(FilterMail.greeting() != null)
-  }
 }
